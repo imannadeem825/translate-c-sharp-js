7b8ba854c88a794d842393da5fbb5cabef9617d3
Iman Nadeem <iman.nadeem.825@gmail.com>
2021-03-01 11:00:35 -0600

    finished place names translation

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,1 +1,29 @@
 // Put your code here
+
+const names = [
+    "Nashville",
+    "Hong Kong",
+    "The back yard",
+    "Earth",
+    "London",
+    "The mall",
+    "Ryman Auditorium",
+    "The Great Wall of China"
+]
+
+console.log("All Place Names")
+
+for (let name of names) {
+    console.log(name)
+}
+
+console.log("")
+
+const theNames = names.filter(n => n.startsWith("The"))
+console.log(theNames)
+
+console.log(" 'The' Place Names")
+for (n of theNames) {
+    console.log(n)
+}
+
