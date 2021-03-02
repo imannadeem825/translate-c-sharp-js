06f1e4f3bcbff020df159bf86de1a2cc366a39ce
Iman Nadeem <iman.nadeem.825@gmail.com>
2021-03-02 09:41:29 -0600

    enemies list working

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -1,1 +1,48 @@
-// Put your code here
+console.log("My Enemies List!")
+console.log("----------------")
+
+
+
+const getEnemies = () => {
+    const enemies = [
+        {
+            firstName: "Joshua",
+            lastName: "Flowers",
+            offenses: [
+                "Being a jerk to me in elementary school",
+                "Not being nice to me in elementary school"
+            ],
+            isReallyHated: true
+        },
+        {
+            firstName: "Darth",
+            lastName: "Vader",
+            offenses: [
+                "Cut off Luke's hand",
+                "Murdered all those kids",
+                "Unkind management practices"
+            ],
+            isReallyHated: false
+        },
+        {
+            firstName: "Leon",
+            lastName: "Peck",
+            offenses: [
+                "Keeps giving me a hotplate"
+            ],
+            isReallyHated: false
+        }
+    ]
+    
+    for (let enemy of enemies) {
+        if (enemy.isReallyHated === true) {
+            console.log(`${enemy.firstName} ${enemy.lastName} Really, really dislike!`)
+        }
+        else {
+            console.log(`${enemy.firstName} ${enemy.lastName}`)
+        }
+    }
+    return enemies
+}
+
+console.log(getEnemies())
